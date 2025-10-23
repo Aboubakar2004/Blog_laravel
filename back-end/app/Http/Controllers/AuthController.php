2b3b3297c02a2723a21bd08f3b45a4e7386ca748
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-10-23 17:36:01 +0200

    ajout de la methode logout dans le fichier AuthController

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -48,4 +48,12 @@ class AuthController extends Controller
             "user" => $user->id
         ]);
     }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            "message" => "Utilisateur déconnecté avec succès",
+        ]);
+    }
 }
