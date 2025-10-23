53370b312257ad33ed8444830993ee208bac7512
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-10-23 16:28:54 +0200

    ajout de la method register dans le fichier AuthController

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function register (Request $request) {
+        $validate = $request->validate([
+            "name" => "required|string|",
+            "email" => "required|string|email|unique:users,email",
+            "password" => "required|string|min:6",
+        ]);
 
+        $validate["password"] = Hash::make($validate["password"]);
+        $user = User::create($validate);
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return response()->json([
+            "message" => "Utilisateur crée avec succès",
+            "user" => $user,
+            "token" => $token
+        ]);
+    }
 }
