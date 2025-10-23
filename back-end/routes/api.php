0c4cfda95e5b27572d37d668854c37c44406ed0d
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-10-23 17:53:47 +0200

    ajout de la methode getUserInfo dans le fichier UserController

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,5 @@ Route::get('/user', function (Request $request) {
 Route::post('/register' , [AuthController::class, "register"]);
 Route::post('/login' , [AuthController::class, "login"]);
 Route::middleware("auth:sanctum")->post('/logout', [AuthController::class, "logout"]);
+Route::middleware("auth:sanctum")->get('/user/name', [UserController::class, "getUserInfo"]);
 
