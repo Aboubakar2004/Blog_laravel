a8a1d60d6685f862e438ee2a42c04e081c3996b1
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-10-25 22:57:21 +0200

    ajout de la fonction login dans le fichier login.tsx

--- front-end/src/pages/Login/Login.tsx
+++ front-end/src/pages/Login/Login.tsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import { loginUser } from "../../api/auth";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const data = await loginUser({
+        email: form.email,
+        password: form.password,
+      });
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        navigate("/userpage");
+      }
+      setMessage(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Mot de passe</label>
+            <input
+              type="password"
+              value={form.password}
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+          </div>
+          <input type="submit" value={"Se connecter"} />
+          {message && <h1 className="text-red-500">{message}</h1>}
+        </form>
+      </div>
     </div>
   );
 }
