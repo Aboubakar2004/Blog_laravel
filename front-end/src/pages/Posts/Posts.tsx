273517e906758dbdbfb1c4b4ffbb62fa057fd18a
Aboubakar2004 <aboubakarbamba718@gmail.com>
2025-10-27 13:40:48 +0100

    Création du formulaire pour la page Posts.tsx

--- front-end/src/pages/Posts/Posts.tsx
+++ front-end/src/pages/Posts/Posts.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function Posts() {
+  const [form, setForm] = useState({ title: "", content: "" });
   return (
     <div>
-      <h1>Posts</h1>
+      <div>
+        <form action="">
+          <div>
+            <label htmlFor="">Titre</label>
+            <input
+              type="text"
+              value={form.title}
+              onChange={(e) => setForm({ ...form, title: e.target.value })}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Contenue</label>
+            <textarea
+              value={form.content}
+              onChange={(e) => setForm({ ...form, content: e.target.value })}
+            ></textarea>
+          </div>
+          <input type="submit" value={"Poster"} />
+        </form>
+      </div>
     </div>
   );
 }
